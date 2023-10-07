d35287c4a63b01731c7485a06abbf8b44696870e
Igor Wilian Faoro <igor.faoro17@gmail.com>
2023-10-07 14:53:39 -0300

    feat: card component; home

---
+++ src/pages/Home/components/Filters/index.tsx
@@ -0,0 +1,10 @@
+import AppCard from '../../../../components/AppCard';
+import './index.scss';
+
+export default function Filters() {
+    return (
+        <div id="home-filters">
+            <AppCard>Filters</AppCard>
+        </div>
+    );
+}
